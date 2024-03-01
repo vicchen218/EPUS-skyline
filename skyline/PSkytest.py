0987f518f95eb65e0bd33ef7767f7b206e9789ae
vicchen218 <syes50804@gmail.com>
2024-03-01 16:58:54 +0800

    init my work PSky.

---
+++ skyline/PSkytest.py
@@ -0,0 +1,6 @@
+# Sliding window brute force PSky
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
+
+if __name__ == '__main__':
+    print("Hello world")
